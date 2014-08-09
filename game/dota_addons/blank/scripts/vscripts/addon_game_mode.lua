9ea8d4d19b4679546dbc2bb5b4e57351464daf52
Alan <nickoladze@gmail.com>
2014-08-09 15:24:10 -0400

    Initial commit

---
+++ game/dota_addons/blank/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,278 @@
+-- TODO
+-- Clean up unused map configs/reading them
+-- UI at game start
+-- Clean up all these functions
+-- Rosh pit vision in spawn area
+-- Allow units to attack once at the fountain
+-- Strings
+-- Snap buildings to grid
+-- Loadout items for heroes spawned
+-- Play a voice line at the start of every wave
+-- "Attempting to create unknown particle system 'RESOURCE_HANDLE_INVALID'" on tower placement
+
+require("holdout_game_round")
+require("holdout_game_spawner")
+
+if CBlankGameMode == nil then
+	CBlankGameMode = class({})
+end
+
+function Precache( context )
+	PrecacheItemByNameSync( "item_tower_basic", context )
+end
+
+function Activate()
+	GameRules.AddonTemplate = CBlankGameMode()
+	GameRules.AddonTemplate:InitGameMode()
+end
+
+function CBlankGameMode:InitGameMode()
+	self._nRoundNumber = 1
+	self._currentRound = nil
+	self._flLastThinkGameTime = nil
+	self._entAncient = Entities:FindByName( nil, "dota_goodguys_fort" )
+	if not self._entAncient then
+		print( "Ancient entity not found!" )
+	end
+
+	self:_ReadGameConfiguration()
+
+	--GameRules:SetTimeOfDay(0.75)
+	GameRules:SetHeroRespawnEnabled(false)
+	GameRules:SetUseUniversalShopMode(true)
+	GameRules:SetHeroSelectionTime(30.0)
+	GameRules:SetPreGameTime(5.0)
+	GameRules:SetSameHeroSelectionEnabled(true)
+	GameRules:SetGoldTickTime(60.0)
+	GameRules:SetGoldPerTick(0)
+
+	GameRules:GetGameModeEntity():SetRemoveIllusionsOnDeath(true)
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride(true)
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible(false)
+	GameRules:GetGameModeEntity():SetFogOfWarDisabled(true)
+	GameRules:GetGameModeEntity():SetRecommendedItemsDisabled(true)
+	GameRules:GetGameModeEntity():SetCameraDistanceOverride(1500)
+	--GameRules:GetGameModeEntity():ClientLoadGridNav()
+
+	SendToServerConsole('jointeam good')
+
+	-- Hook into game events allowing reload of functions at run time
+	ListenToGameEvent( "npc_spawned", Dynamic_Wrap( CBlankGameMode, "OnNPCSpawned" ), self )
+	ListenToGameEvent( "player_reconnected", Dynamic_Wrap( CBlankGameMode, 'OnPlayerReconnected' ), self )
+	--ListenToGameEvent( "entity_killed", Dynamic_Wrap( CBlankGameMode, 'OnEntityKilled' ), self )
+	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( CBlankGameMode, "OnGameRulesStateChange" ), self )
+	
+	GameRules:GetGameModeEntity():SetThink("OnThink", self, 0.25)
+end
+
+-- Read and assign configurable keyvalues if applicable
+function CBlankGameMode:_ReadGameConfiguration()
+	local kv = LoadKeyValues( "scripts/maps/" .. GetMapName() .. ".txt" )
+	kv = kv or {} -- Handle the case where there is not keyvalues file
+
+	self._bAlwaysShowPlayerGold = kv.AlwaysShowPlayerGold or false
+	self._bRestoreHPAfterRound = kv.RestoreHPAfterRound or false
+	self._bRestoreMPAfterRound = kv.RestoreMPAfterRound or false
+	self._bRewardForTowersStanding = kv.RewardForTowersStanding or false
+	self._bUseReactiveDifficulty = kv.UseReactiveDifficulty or false
+
+	self._nTowerRewardAmount = tonumber( kv.TowerRewardAmount or 0 )
+	self._nTowerScalingRewardPerRound = tonumber( kv.TowerScalingRewardPerRound or 0 )
+
+	self._flPrepTimeBetweenRounds = tonumber( kv.PrepTimeBetweenRounds or 0 )
+	self._flItemExpireTime = tonumber( kv.ItemExpireTime or 10.0 )
+
+	self:_ReadSpawnsConfiguration( kv["Spawns"] )
+	--self:_ReadLootItemDropsConfiguration( kv["ItemDrops"] )
+	self:_ReadRoundConfigurations( kv )
+end
+
+-- Verify spawners if random is set
+function CBlankGameMode:ChooseRandomSpawnInfo()
+	if #self._vSpawnsList == 0 then
+		error( "Attempt to choose a random spawn, but no random spawns are specified in the data." )
+		return nil
+	end
+	return self._vSpawnsList[ RandomInt( 1, #self._vSpawnsList ) ]
+end
+
+-- Verify valid spawns are defined and build a table with them from the keyvalues file
+function CBlankGameMode:_ReadSpawnsConfiguration( kvSpawns )
+	self._vSpawnsList = {}
+	if type( kvSpawns ) ~= "table" then
+		return
+	end
+	for _,sp in pairs( kvSpawns ) do			-- Note "_" used as a shortcut to create a temporary throwaway variable
+		table.insert( self._vSpawnsList, {
+			szSpawnerName = sp.SpawnerName or "",
+			szFirstWaypoint = sp.Waypoint or ""
+		} )
+	end
+end
+
+-- Set number of rounds without requiring index in text file
+function CBlankGameMode:_ReadRoundConfigurations( kv )
+	self._vRounds = {}
+	while true do
+		local szRoundName = string.format("Round%d", #self._vRounds + 1 )
+		local kvRoundData = kv[ szRoundName ]
+		if kvRoundData == nil then
+			return
+		end
+		local roundObj = CHoldoutGameRound()
+		roundObj:ReadConfiguration( kvRoundData, self, #self._vRounds + 1 )
+		table.insert( self._vRounds, roundObj )
+	end
+end
+
+-- When game state changes set state in script
+function CBlankGameMode:OnGameRulesStateChange()
+	local nNewState = GameRules:State_Get()
+	if nNewState == DOTA_GAMERULES_STATE_PRE_GAME then
+		ShowGenericPopup( "#holdout_instructions_title", "#holdout_instructions_body", "", "", DOTA_SHOWGENERICPOPUP_TINT_SCREEN )
+	elseif nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
+	end
+end
+
+function CBlankGameMode:OnThink()
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		self:_CheckForDefeat()
+
+		if self._flPrepTimeEnd ~= nil then
+			self:_ThinkPrepTime()
+		elseif self._currentRound ~= nil then
+			self._currentRound:Think()
+			if self._currentRound:IsFinished() then
+				self._currentRound:End()
+				self._currentRound = nil
+				-- Heal all players
+				--self:_RefreshPlayers()
+
+				self._nRoundNumber = self._nRoundNumber + 1
+				if self._nRoundNumber > #self._vRounds then
+					self._nRoundNumber = 1
+					GameRules:MakeTeamLose( DOTA_TEAM_BADGUYS )
+				else
+					self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
+				end
+			end
+		end
+	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+		return nil
+	end
+	return 1
+end
+
+function CBlankGameMode:_CheckForDefeat()
+	if GameRules:State_Get() ~= DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		return
+	end
+
+	local bAllPlayersDead = true
+	for nPlayerID = 0, DOTA_MAX_TEAM_PLAYERS-1 do
+		if PlayerResource:GetTeam( nPlayerID ) == DOTA_TEAM_GOODGUYS then
+			if not PlayerResource:HasSelectedHero( nPlayerID ) then
+				bAllPlayersDead = false
+			else
+				local hero = PlayerResource:GetSelectedHeroEntity( nPlayerID )
+				if hero and hero:IsAlive() then
+					bAllPlayersDead = false
+				end
+			end
+		end
+	end
+
+	if bAllPlayersDead or not self._entAncient or self._entAncient:GetHealth() <= 0 then
+		GameRules:MakeTeamLose( DOTA_TEAM_GOODGUYS )
+		return
+	end
+end
+
+function CBlankGameMode:_ThinkPrepTime()
+	if GameRules:GetGameTime() >= self._flPrepTimeEnd then
+		self._flPrepTimeEnd = nil
+		if self._entPrepTimeQuest then
+			UTIL_RemoveImmediate( self._entPrepTimeQuest )
+			self._entPrepTimeQuest = nil
+		end
+
+		if self._nRoundNumber > #self._vRounds then
+			GameRules:SetGameWinner( DOTA_TEAM_GOODGUYS )
+			return false
+		end
+		self._currentRound = self._vRounds[ self._nRoundNumber ]
+		self._currentRound:Begin()
+		return
+	end
+
+	if not self._entPrepTimeQuest then
+		self._entPrepTimeQuest = SpawnEntityFromTableSynchronous( "quest", { name = "PrepTime", title = "#DOTA_Quest_Holdout_PrepTime" } )
+		self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_ROUND, self._nRoundNumber )
+		self._entPrepTimeQuest:SetTextReplaceString( self:GetDifficultyString() )
+
+		self._vRounds[ self._nRoundNumber ]:Precache()
+	end
+	self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_CURRENT_VALUE, self._flPrepTimeEnd - GameRules:GetGameTime() )
+end
+
+function CBlankGameMode:GetDifficultyString()
+	local nDifficulty = GameRules:GetCustomGameDifficulty()
+	if nDifficulty > 4 then
+		return string.format( "(+%d)", nDifficulty )
+	elseif nDifficulty > 0 then
+		return string.rep( "+", nDifficulty )
+	else
+		return ""
+	end
+end
+
+function CBlankGameMode:OnNPCSpawned( event )
+	local spawnedUnit = EntIndexToHScript( event.entindex )
+	if not spawnedUnit or spawnedUnit:GetClassname() == "npc_dota_thinker" or spawnedUnit:IsPhantom() then
+		return
+	end
+
+	if spawnedUnit:IsCreature() then
+		spawnedUnit:SetHPGain( spawnedUnit:GetMaxHealth() * 0.3 ) -- LEVEL SCALING VALUE FOR HP
+		spawnedUnit:SetManaGain( 0 )
+		spawnedUnit:SetHPRegenGain( 0 )
+		spawnedUnit:SetManaRegenGain( 0 )
+		if spawnedUnit:IsRangedAttacker() then
+			spawnedUnit:SetDamageGain( ( ( spawnedUnit:GetBaseDamageMax() + spawnedUnit:GetBaseDamageMin() ) / 2 ) * 0.1 ) -- LEVEL SCALING VALUE FOR DPS
+		else
+			spawnedUnit:SetDamageGain( ( ( spawnedUnit:GetBaseDamageMax() + spawnedUnit:GetBaseDamageMin() ) / 2 ) * 0.2 ) -- LEVEL SCALING VALUE FOR DPS
+		end
+		spawnedUnit:SetArmorGain( 0 )
+		spawnedUnit:SetMagicResistanceGain( 0 )
+		spawnedUnit:SetDisableResistanceGain( 0 )
+		spawnedUnit:SetAttackTimeGain( 0 )
+		spawnedUnit:SetMoveSpeedGain( 0 )
+		spawnedUnit:SetBountyGain( 0 )
+		spawnedUnit:SetXPGain( 0 )
+		spawnedUnit:CreatureLevelUp( GameRules:GetCustomGameDifficulty() )
+	elseif spawnedUnit:IsRealHero() and spawnedUnit:IsOwnedByAnyPlayer() then
+		spawnedUnit:AddAbility('riki_permanent_invisibility')
+		spawnedUnit:FindAbilityByName('riki_permanent_invisibility'):SetLevel(3)
+	
+		for i=1,24,1 do
+			spawnedUnit:HeroLevelUp(false)
+		end
+
+		spawnedUnit:SetAbilityPoints(0)
+	end
+end
+
+-- TODO delete?
+function CBlankGameMode:OnEntityKilled( event )
+	local killedUnit = EntIndexToHScript( event.entindex_killed )
+	if killedUnit and killedUnit:IsRealHero() then
+		local newItem = CreateItem( "item_tombstone", killedUnit, killedUnit )
+		newItem:SetPurchaseTime( 0 )
+		newItem:SetPurchaser( killedUnit )
+		local tombstone = SpawnEntityFromTableSynchronous( "dota_item_tombstone_drop", {} )
+		tombstone:SetContainedItem( newItem )
+		tombstone:SetAngles( 0, RandomFloat( 0, 360 ), 0 )
+		FindClearSpaceForUnit( tombstone, killedUnit:GetAbsOrigin(), true )	
+	end
+end
---
+++ game/dota_addons/blank/scripts/vscripts/holdout_game_round.lua
@@ -0,0 +1,256 @@
+--[[
+	CHoldoutGameRound - A single round of Holdout
+]]
+
+if CHoldoutGameRound == nil then
+	CHoldoutGameRound = class({})
+end
+
+
+function CHoldoutGameRound:ReadConfiguration( kv, gameMode, roundNumber )
+	self._gameMode = gameMode
+	self._nRoundNumber = roundNumber
+	self._szRoundQuestTitle = kv.round_quest_title or "#DOTA_Quest_Holdout_Round"
+	self._szRoundTitle = kv.round_title or string.format( "Round%d", roundNumber )
+
+	--self._nMaxGold = tonumber( kv.MaxGold or 0 )
+	--self._nBagCount = tonumber( kv.BagCount or 0 )
+	--self._nBagVariance = tonumber( kv.BagVariance or 0 )
+	--self._nFixedXP = tonumber( kv.FixedXP or 0 )
+
+	self._vSpawners = {}
+	for k, v in pairs( kv ) do
+		if type( v ) == "table" and v.NPCName then
+			local spawner = CHoldoutGameSpawner()
+			spawner:ReadConfiguration( k, v, self )
+			self._vSpawners[ k ] = spawner
+		end
+	end
+
+	for _, spawner in pairs( self._vSpawners ) do
+		spawner:PostLoad( self._vSpawners )
+	end
+end
+
+
+function CHoldoutGameRound:Precache()
+	for _, spawner in pairs( self._vSpawners ) do
+		spawner:Precache()
+	end
+end
+
+function CHoldoutGameRound:Begin()
+	self._vEnemiesRemaining = {}
+	self._vEventHandles = {
+		ListenToGameEvent( "npc_spawned", Dynamic_Wrap( CHoldoutGameRound, "OnNPCSpawned" ), self ),
+		ListenToGameEvent( "entity_killed", Dynamic_Wrap( CHoldoutGameRound, "OnEntityKilled" ), self ),
+		ListenToGameEvent( "dota_item_picked_up", Dynamic_Wrap( CHoldoutGameRound, 'OnItemPickedUp' ), self ),
+		ListenToGameEvent( "dota_holdout_revive_complete", Dynamic_Wrap( CHoldoutGameRound, 'OnHoldoutReviveComplete' ), self )
+	}
+
+	self._vPlayerStats = {}
+	for nPlayerID = 0, DOTA_MAX_TEAM_PLAYERS-1 do
+		self._vPlayerStats[ nPlayerID ] = {
+			nCreepsKilled = 0,
+			nGoldBagsCollected = 0,
+			nPriorRoundDeaths = PlayerResource:GetDeaths( nPlayerID ),
+			nPlayersResurrected = 0
+		}
+	end
+
+	--self._nGoldRemainingInRound = self._nMaxGold
+	--self._nGoldBagsRemaining = self._nBagCount
+	--self._nGoldBagsExpired = 0
+	self._nCoreUnitsTotal = 0
+	for _, spawner in pairs( self._vSpawners ) do
+		spawner:Begin()
+		self._nCoreUnitsTotal = self._nCoreUnitsTotal + spawner:GetTotalUnitsToSpawn()
+	end
+	self._nCoreUnitsKilled = 0
+
+	self._entQuest = SpawnEntityFromTableSynchronous( "quest", {
+		name = self._szRoundTitle,
+		title =  self._szRoundQuestTitle
+	})
+	self._entQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_ROUND, self._nRoundNumber )
+	self._entQuest:SetTextReplaceString( self._gameMode:GetDifficultyString() )
+
+	self._entKillCountSubquest = SpawnEntityFromTableSynchronous( "subquest_base", {
+		show_progress_bar = true,
+		progress_bar_hue_shift = -119
+	} )
+	self._entQuest:AddSubquest( self._entKillCountSubquest )
+	self._entKillCountSubquest:SetTextReplaceValue( SUBQUEST_TEXT_REPLACE_VALUE_TARGET_VALUE, self._nCoreUnitsTotal )
+end
+
+
+function CHoldoutGameRound:End()
+	for _, eID in pairs( self._vEventHandles ) do
+		StopListeningToGameEvent( eID )
+	end
+	self._vEventHandles = {}
+
+	for _,unit in pairs( FindUnitsInRadius( DOTA_TEAM_BADGUYS, Vector( 0, 0, 0 ), nil, FIND_UNITS_EVERYWHERE, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )) do
+		if not unit:IsTower() then
+			UTIL_RemoveImmediate( unit )
+		end
+	end
+
+	for _,spawner in pairs( self._vSpawners ) do
+		spawner:End()
+	end
+
+	if self._entQuest then
+		UTIL_RemoveImmediate( self._entQuest )
+		self._entQuest = nil
+		self._entKillCountSubquest = nil
+	end
+
+	local nTowers = 0
+	local nTowersStanding = 0
+	for _,unit in pairs( FindUnitsInRadius( DOTA_TEAM_GOODGUYS, Vector( 0, 0, 0 ), nil, FIND_UNITS_EVERYWHERE, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BUILDING, DOTA_UNIT_TARGET_FLAG_DEAD, FIND_ANY_ORDER, false ) ) do
+		if unit:IsTower() then
+			nTowers = nTowers + 1
+			if unit:IsAlive() then
+				nTowersStanding = nTowersStanding + 1
+			end
+		end
+	end
+	--local nTowersStandingGoldReward = self._gameMode:ComputeTowerBonusGold( nTowers, nTowersStanding )
+	--for nPlayerID = 0, DOTA_MAX_TEAM_PLAYERS-1 do
+	--	if PlayerResource:HasSelectedHero( nPlayerID ) then
+	--		PlayerResource:ModifyGold( nPlayerID, nTowersStandingGoldReward, true, DOTA_ModifyGold_Unspecified )
+	--	end
+	--end
+
+	local roundEndSummary = {
+		nRoundNumber = self._nRoundNumber - 1,
+		nRoundDifficulty = GameRules:GetCustomGameDifficulty(),
+		roundName = self._szRoundTitle,
+		nTowers = nTowers,
+		nTowersStanding = nTowersStanding,
+		--nTowersStandingGoldReward = nTowersStandingGoldReward,
+		nGoldBagsExpired = self._nGoldBagsExpired
+	}
+
+	local playerSummaryCount = 0
+	for i = 1, DOTA_MAX_TEAM_PLAYERS do
+		local nPlayerID = i-1
+		if PlayerResource:HasSelectedHero( nPlayerID ) then
+			local szPlayerPrefix = string.format( "Player_%d_", playerSummaryCount)
+			playerSummaryCount = playerSummaryCount + 1
+			local playerStats = self._vPlayerStats[ nPlayerID ]
+			roundEndSummary[ szPlayerPrefix .. "HeroName" ] = PlayerResource:GetSelectedHeroName( nPlayerID )
+			roundEndSummary[ szPlayerPrefix .. "CreepKills" ] = playerStats.nCreepsKilled
+			roundEndSummary[ szPlayerPrefix .. "GoldBagsCollected" ] = playerStats.nGoldBagsCollected
+			roundEndSummary[ szPlayerPrefix .. "Deaths" ] = PlayerResource:GetDeaths( nPlayerID ) - playerStats.nPriorRoundDeaths
+			roundEndSummary[ szPlayerPrefix .. "PlayersResurrected" ] = playerStats.nPlayersResurrected
+		end
+	end
+	FireGameEvent( "holdout_show_round_end_summary", roundEndSummary )
+end
+
+
+function CHoldoutGameRound:Think()
+	for _, spawner in pairs( self._vSpawners ) do
+		spawner:Think()
+	end
+end
+
+
+function CHoldoutGameRound:ChooseRandomSpawnInfo()
+	return self._gameMode:ChooseRandomSpawnInfo()
+end
+
+
+function CHoldoutGameRound:IsFinished()
+	for _, spawner in pairs( self._vSpawners ) do
+		if not spawner:IsFinishedSpawning() then
+			return false
+		end
+	end
+	local nEnemiesRemaining = #self._vEnemiesRemaining
+	if nEnemiesRemaining == 0 then
+		return true
+	end
+
+	if not self._lastEnemiesRemaining == nEnemiesRemaining then
+		self._lastEnemiesRemaining = nEnemiesRemaining
+		print ( string.format( "%d enemies remaining in the round...", #self._vEnemiesRemaining ) )
+	end
+	return false
+end
+
+
+function CHoldoutGameRound:GetXPPerCoreUnit()
+	return 0
+end
+
+
+function CHoldoutGameRound:OnNPCSpawned( event )
+	local spawnedUnit = EntIndexToHScript( event.entindex )
+	if not spawnedUnit or spawnedUnit:IsPhantom() or spawnedUnit:GetClassname() == "npc_dota_thinker" or spawnedUnit:GetUnitName() == "" then
+		return
+	end
+
+	if spawnedUnit:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+		spawnedUnit:SetMustReachEachGoalEntity(true)
+		table.insert( self._vEnemiesRemaining, spawnedUnit )
+		spawnedUnit:SetDeathXP( 0 )
+		spawnedUnit.unitName = spawnedUnit:GetUnitName()
+	end
+end
+
+
+function CHoldoutGameRound:OnEntityKilled( event )
+	local killedUnit = EntIndexToHScript( event.entindex_killed )
+	if not killedUnit then
+		return
+	end
+
+	for i, unit in pairs( self._vEnemiesRemaining ) do
+		if killedUnit == unit then
+			table.remove( self._vEnemiesRemaining, i )
+			break
+		end
+	end	
+	if killedUnit.Holdout_IsCore then
+		self._nCoreUnitsKilled = self._nCoreUnitsKilled + 1
+		--self:_CheckForGoldBagDrop( killedUnit )
+		--self._gameMode:CheckForLootItemDrop( killedUnit )
+		if self._entKillCountSubquest then
+			self._entKillCountSubquest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_CURRENT_VALUE, self._nCoreUnitsKilled )
+		end
+	end
+
+	local attackerUnit = EntIndexToHScript( event.entindex_attacker or -1 )
+	if attackerUnit then
+		local playerID = attackerUnit:GetPlayerOwnerID()
+		local playerStats = self._vPlayerStats[ playerID ]
+		if playerStats then
+			playerStats.nCreepsKilled = playerStats.nCreepsKilled + 1
+		end
+	end
+end
+
+
+function CHoldoutGameRound:OnHoldoutReviveComplete( event )
+	local castingHero = EntIndexToHScript( event.caster )
+	if castingHero then
+		local nPlayerID = castingHero:GetPlayerOwnerID()
+		local playerStats = self._vPlayerStats[ nPlayerID ]
+		if playerStats then
+			playerStats.nPlayersResurrected = playerStats.nPlayersResurrected + 1
+		end
+	end
+end
+
+
+function CHoldoutGameRound:OnItemPickedUp( event )
+	if event.itemname == "item_bag_of_gold" then
+		local playerStats = self._vPlayerStats[ event.PlayerID ]
+		if playerStats then
+			playerStats.nGoldBagsCollected = playerStats.nGoldBagsCollected + 1
+		end
+	end
+end
